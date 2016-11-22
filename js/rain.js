26464b72e2142e207944f34be18e395fbca52dc7
ccoggins16 <christoph.coggins@gmail.com>
2016-11-22 02:11:02 -0800

    added stopRain function to rain.js, addded call in html

--- js/rain.js
+++ js/rain.js
@@ -5,12 +5,18 @@ function randRange(minNum, maxNum) {
 }
 
 function createRain() {
-  
+  $('.drop').css('display',"block");
   for( i=1;i<nbDrop;i++) {
     var dropLeft = randRange(0,1600);
     var dropTop = randRange(-1000,1400);
     $('.rain').append('<div class="drop" id="drop'+i+'"></div>');
     $('#drop'+i).css('left',dropLeft);
     $('#drop'+i).css('top',dropTop);
+    
+    
   }
+}
+
+function stopRain() {
+  $('.drop').css('display',"none");
 }
